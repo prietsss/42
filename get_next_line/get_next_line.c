bd402da3e3e4edecc689b745baf6624b436760ef
prietsss <alexpriets.11@gmail.com>
2026-09-08 00:29:43 +0200

    Add header comments and new files for ft_printf and get_next_line modules

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,30 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alepriet <alepriet@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/09/08 00:29:05 by alepriet          #+#    #+#             */
-/*   Updated: 2026/09/08 00:29:06 by alepriet         ###   ########.fr       */
+/*   Created: 2026/09/08 00:27:52 by alepriet          #+#    #+#             */
+/*   Updated: 2026/09/08 00:28:05 by alepriet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-#define FT_PRINTF_H
-
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-int ft_printf(const char *format, ...);
-
-int ft_print_char(int c);
-int ft_print_str(char *str);
-int ft_print_ptr(void *ptr);
-int ft_print_nbr(int n);
-int ft_print_unsigned(unsigned int n);
-int ft_print_hex(unsigned int n, char format);
-
-#endif
