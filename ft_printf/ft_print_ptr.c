1c44de61973a39157d939b4f6b7d93396de8cf84
Alejandro Prieto <117353645+prietsss@users.noreply.github.com>
2026-09-22 19:58:15 +0200

    Add files via upload

---
+++ ft_printf/ft_print_ptr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_ptr.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alepriet <alepriet@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/08 00:28:37 by alepriet          #+#    #+#             */
+/*   Updated: 2026/09/08 00:28:38 by alepriet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static int	ft_put_ptr(unsigned long long n)
+{
+	int	count;
+
+	count = 0;
+	if (n >= 16)
+		count += ft_put_ptr(n / 16);
+	count += ft_print_char("0123456789abcdef"[n % 16]);
+	return (count);
+}
+
+int	ft_print_ptr(void *ptr)
+{
+	int	count;
+
+	count = 0;
+	if (!ptr)
+		return (write(1, "(nil)", 5));
+	count += write(1, "0x", 2);
+	count += ft_put_ptr((unsigned long long)ptr);
+	return (count);
+}
